c6aff69b4aa5c6e906d197202585ae48ed0d6259
feizheng <1290657123@qq.com>
2016-07-12 01:05:23 +0800

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# generator-ionic-wechat-app [![NPM version][npm-image]][npm-url] [![Build Status][travis-image]][travis-url] [![Dependency Status][daviddm-image]][daviddm-url]
+> Yeoman generator for ionic-wechat-app.
+
+## Installation
+
+First, install [Yeoman](http://yeoman.io) and generator-ionic-wechat-app using [npm](https://www.npmjs.com/) (we assume you have pre-installed [node.js](https://nodejs.org/)).
+
+```bash
+npm install -g yo
+npm install -g generator-ionic-wechat-app
+```
+
+Then generate your new project:
+
+```bash
+yo ionic-wechat-app
+```
+
+## Getting To Know Yeoman
+
+ * Yeoman has a heart of gold.
+ * Yeoman is a person with feelings and opinions, but is very easy to work with.
+ * Yeoman can be too opinionated at times but is easily convinced not to be.
+ * Feel free to [learn more about Yeoman](http://yeoman.io/).
+
+## License
+
+MIT © [feizheng](https://github.com/afeiship)
+
+
+[npm-image]: https://badge.fury.io/js/generator-ionic-wechat-app.svg
+[npm-url]: https://npmjs.org/package/generator-ionic-wechat-app
+[travis-image]: https://travis-ci.org/afeiship/generator-ionic-wechat-app.svg?branch=master
+[travis-url]: https://travis-ci.org/afeiship/generator-ionic-wechat-app
+[daviddm-image]: https://david-dm.org/afeiship/generator-ionic-wechat-app.svg?theme=shields.io
+[daviddm-url]: https://david-dm.org/afeiship/generator-ionic-wechat-app
---
+++ generators/app/index.js
@@ -0,0 +1,79 @@
+'use strict';
+var yeoman = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+var path = require('path');
+var ejs = require('ejs');
+var _ = require('underscore');
+var rExt = /^\.(js|json|scss|sass|css|html|md)$/i;
+
+
+module.exports = yeoman.Base.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the delightful ' + chalk.red('generator-test-yo') + ' generator!'
+    ));
+    
+    var prompts = [{
+      type: 'input',
+      name: 'project_name',
+      message: 'Your project_name?',
+      default: function () {
+        var cwd = path.basename(process.cwd());
+        var result = cwd.split(/[-_]/);
+        result.shift();
+        return result.join('-');
+      }
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this._rewriteProps();
+    this._fetchFromGit();
+  },
+  install: function () {
+    //this.installDependencies();
+    console.log('bower install && npm install');
+  },
+  _rewriteProps: function () {
+    var props = this.props;
+    _.each(props, function (prop, key) {
+      this.props[this._camelCase(key)] = this._camelCase(prop);
+    }, this);
+  },
+  _camelCase: function (inString) {
+    return inString.replace(/[_-]\D/g, function (match) {
+      return match.charAt(1).toUpperCase();
+    });
+  },
+  _fetchFromGit: function () {
+    var self = this;
+    this.remote('afeiship', 'ionic-wechat-app', 'master', function (err, remote) {
+      self._extCopyTpl(path.join(remote.cachePath, './{**,.*}'), './');
+    });
+  },
+  _extCopyTpl: function (inSrc, inDest) {
+    var self = this;
+    this.fs.copy(inSrc, inDest, {
+        process: function (contents, filename) {
+          var extname = path.extname(filename);
+          if (rExt.test(extname)) {
+            return ejs.render(
+              contents.toString(), self.props, {
+                filename: filename
+              }
+            );
+          } else {
+            return contents;
+          }
+        }
+      }
+    );
+  }
+});
